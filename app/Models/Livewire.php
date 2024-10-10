28ec6357f0f1a4489c0892644a37aa2998391517
andresperezfc <felipe3apc@gmail.com>
2024-10-09 22:39:06 -0500

    Creación de modelo Livewire

---
+++ app/Models/Livewire.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Livewire extends Model
+{
+    use HasFactory;
+}
